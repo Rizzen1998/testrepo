6bfd7802ff8e1002efe1bebabb8c2cc25b28b170
Jennifer Clark <jennifer.c.schultz@gmail.com>
2021-10-02 17:48:04 -0600

    this modifies the main file and adds a name function

--- Tyyr1/main.py
+++ Tyyr1/main.py
@@ -1,1 +1,12 @@
+
+
 print('Welcome to our collaborative program')
+
+# this main function will hold functions from other files. 
+def main():
+    #add name.py function
+    
+    
+#calls the main function
+if __name__ == "__main__":
+    main()    
---
+++ Tyyr1/name.py
@@ -0,0 +1,4 @@
+def myname():
+    print('My name is Tyyr')
+
+myname()
